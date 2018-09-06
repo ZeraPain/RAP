6b00f7c52fd68be86f944013de4cd80e37511f40
raplab <dennisweilinger@gmail.com>
2018-09-06 16:03:20 +0200

    added Book and bookOnStock

---
+++ library-server/src/main/java/de/h_da/library/datamanagement/entity/BookOnStock.java
@@ -0,0 +1,16 @@
+package de.h_da.library.datamanagement.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BookOnStock implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Book book;
+}
