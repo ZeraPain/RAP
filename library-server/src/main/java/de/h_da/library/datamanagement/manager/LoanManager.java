f1b2fe66d12e1b78d317c291acb4b495b4c3ea5d
David <davewayne@gmx.de>
2018-09-06 16:28:06 +0200

    added LoanManager

---
+++ library-server/src/main/java/de/h_da/library/datamanagement/manager/LoanManager.java
@@ -0,0 +1,23 @@
+/*
+ * Entity1FacadeLocal.java
+ *
+ * Created on 10. September 2007, 14:59
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package de.h_da.library.datamanagement.manager;
+
+import javax.ejb.Local;
+import de.h_da.library.datamanagement.entity.Loan;
+
+/**
+ *
+ * @author humm
+ */
+@Local
+public interface LoanManager {
+
+    
+}
