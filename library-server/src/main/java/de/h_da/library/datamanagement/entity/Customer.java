61af293edd7401961e4dd0ffd26057e2d24e1849
raplab <dennisweilinger@gmail.com>
2018-09-06 17:41:39 +0200

    Customer Entity

--- library-server/src/main/java/de/h_da/library/datamanagement/entity/Customer.java
+++ library-server/src/main/java/de/h_da/library/datamanagement/entity/Customer.java
@@ -8,13 +8,15 @@
 
 package de.h_da.library.datamanagement.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Customer {
+public class Customer implements Serializable{
   
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
