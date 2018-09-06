4c350fd169e56ae94035027d292e5328bc174bf2
raplab <dennisweilinger@gmail.com>
2018-09-06 17:08:59 +0200

    Book and BookOnStock impl

--- library-server/src/main/java/de/h_da/library/datamanagement/manager/BookManager.java
+++ library-server/src/main/java/de/h_da/library/datamanagement/manager/BookManager.java
@@ -1,8 +1,19 @@
 package de.h_da.library.datamanagement.manager;
 
+import java.util.List;
+import de.h_da.library.datamanagement.entity.Book;
 import javax.ejb.Local;
+
 
 @Local
 public interface BookManager {
+    void edit(Book book);
 
+    void destroy(Book book);
+
+    Book findById(Long id);
+
+    List<Book> findAll();
+
+    Book create(Book book);
 }
--- library-server/src/main/java/de/h_da/library/datamanagement/manager/BookOnStockManager.java
+++ library-server/src/main/java/de/h_da/library/datamanagement/manager/BookOnStockManager.java
@@ -1,8 +1,21 @@
 package de.h_da.library.datamanagement.manager;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import de.h_da.library.datamanagement.entity.BookOnStock;;
 
 @Local
 public interface BookOnStockManager {
 
+    void edit(BookOnStock bookOnStock);
+
+    void destroy(BookOnStock bookOnStock);
+
+    BookOnStock findById(Long id);
+
+    List<BookOnStock> findAll();
+
+    BookOnStock create(BookOnStock bookOnStock);
 }
