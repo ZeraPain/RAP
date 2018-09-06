95c56df124ae151696c1831563f42cdfa1f6b7d1
raplab <dennisweilinger@gmail.com>
2018-09-06 17:40:37 +0200

    added Customer

--- library-server/src/main/java/de/h_da/library/datamanagement/entity/Customer.java
+++ library-server/src/main/java/de/h_da/library/datamanagement/entity/Customer.java
@@ -8,8 +8,35 @@
 
 package de.h_da.library.datamanagement.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
-    
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  Long id;
+  String name;
+  String address;
+  
+  
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
     
 }
