b80c11f100ea5105dae4b2df2804e2bc8856fe06
Thomas <tbialas@gmx.de>
2018-09-12 02:01:56 +0200

    changed SearchImpl logic and wrote new SearchImplTests

--- library-server/src/main/java/de/h_da/library/datamanagement/usecase/impl/SearchImpl.java
+++ library-server/src/main/java/de/h_da/library/datamanagement/usecase/impl/SearchImpl.java
@@ -29,76 +29,33 @@ public class SearchImpl implements Search, SearchRemote {
 	@Override
 	public List<Book> findBooksByAttributes(Book book) {		
 		List<Book> books = bookManager.findAll();
-		System.out.println("### DEBUG " + this.getClass().toString()+ " books.size: " + books.size());
-		//ArrayList<Book> foundBooks = new ArrayList<Book>();
 		List<Book> foundBooks = books;
-		if(book != null) {
-			//remove all books that are not matching the title or the author
-			/*
-			if(!book.getTitle().isEmpty())
-			{
-				books.removeIf(b->(b.getTitle().isEmpty() || !b.getTitle().toLowerCase().equals(book.getTitle().toLowerCase())));
-			}
-			
-			if(!book.getAuthors().isEmpty())
-				books.removeIf(b->!b.getAuthors().isEmpty() && b.getAuthors().toUpperCase() != book.getAuthors().toUpperCase());
-			*/
-			
+		if(book != null) {			
 			if(book.getTitle() != null && book.getAuthors() == null)
 				foundBooks = books.stream().filter(b->b.getTitle()!=null && !b.getTitle().isEmpty() && b.getTitle().toUpperCase().equals(book.getTitle().toUpperCase())).collect(Collectors.toList());
 			else if(book.getTitle() == null && book.getAuthors() != null)
 				foundBooks = books.stream().filter(b->b.getAuthors()!=null && !b.getAuthors().isEmpty() && b.getAuthors().toUpperCase().equals(book.getAuthors().toUpperCase())).collect(Collectors.toList());
 			else if(book.getTitle() != null && book.getAuthors() != null)
 				foundBooks = books.stream().filter(b->b.getTitle() != null && b.getAuthors() != null && !b.getTitle().isEmpty() && b.getTitle().toUpperCase().equals(book.getTitle().toUpperCase()) 
-				|| !b.getAuthors().isEmpty() && b.getAuthors().toUpperCase().equals(book.getAuthors().toUpperCase())).collect(Collectors.toList());
-				
-			/*
-			for(Book b: books) {
-				if(book.getTitle() != null && book.getAuthors() == null) {
-					if(b.getTitle() == book.getTitle())
-						foundBooks.add(b);
-				}else if(book.getTitle() == null && book.getAuthors() != null) {
-					if(b.getAuthors() == book.getAuthors())
-						foundBooks.add(b);
-				}else if(book.getTitle() != null && book.getAuthors() != null) {
-					if(b.getTitle() == book.getTitle()||b.getAuthors() == book.getAuthors())
-						foundBooks.add(b);
-				}	
-			}*/
+				&& !b.getAuthors().isEmpty() && b.getAuthors().toUpperCase().equals(book.getAuthors().toUpperCase())).collect(Collectors.toList());
+			
 		}
 		return foundBooks;
 	}
 
 	@Override
-	public List<Customer> findCustomersByAttributes(Customer customer) {
-		/*List<Customer> customers = customerManager.findAll();
-		if(customer != null) {
-			//remove all customers that are not matching the name or the address
-			if(!customer.getName().isEmpty())
-				customers.removeIf(c->!c.getName().isEmpty() && c.getName().toUpperCase() != customer.getName().toUpperCase());
-			if(!customer.getAddress().isEmpty())
-				customers.removeIf(c->!c.getAddress().isEmpty() && c.getAddress().toUpperCase() != customer.getAddress().toUpperCase());							
-		}
-		return customers;
-		*/
+	public List<Customer> findCustomersByAttributes(Customer customer) {		
 		List<Customer> customers = customerManager.findAll();
-		List<Customer> foundCustomer = customers;
-		if(customer != null) {
-			//remove all books that are not matching the title or the author
-			/*
-			if(!book.getTitle().isEmpty())
-				books.removeIf(b->!b.getTitle().isEmpty() && b.getTitle().toUpperCase() != book.getTitle().toUpperCase());
-			if(!book.getAuthors().isEmpty())
-				books.removeIf(b->!b.getAuthors().isEmpty() && b.getAuthors().toUpperCase() != book.getAuthors().toUpperCase());
-			*/
+		List<Customer> foundCustomers = customers;
+		if(customer != null) {			
 			if(customer.getName() != null && customer.getAddress() == null)
-				foundCustomer = customers.stream().filter(c->!c.getName().isEmpty() && c.getName().toUpperCase() == customer.getName().toUpperCase()).collect(Collectors.toList());
+				foundCustomers = customers.stream().filter(c->!c.getName().isEmpty() && c.getName().toUpperCase().equals(customer.getName().toUpperCase())).collect(Collectors.toList());
 			else if(customer.getName() == null && customer.getAddress() != null)
-				foundCustomer = customers.stream().filter(c->!c.getAddress().isEmpty() && c.getAddress().toUpperCase() == customer.getAddress().toUpperCase()).collect(Collectors.toList());
+				foundCustomers = customers.stream().filter(c->!c.getAddress().isEmpty() && c.getAddress().toUpperCase().equals(customer.getAddress().toUpperCase())).collect(Collectors.toList());
 			else if(customer.getName() != null && customer.getAddress() != null)
-				foundCustomer = customers.stream().filter(c->!c.getName().isEmpty() && c.getName().toUpperCase() == customer.getName().toUpperCase() 
-				&& !c.getAddress().isEmpty() && c.getAddress().toUpperCase() == customer.getAddress().toUpperCase()).collect(Collectors.toList());
+				foundCustomers = customers.stream().filter(c->!c.getName().isEmpty() && c.getName().toUpperCase().equals(customer.getName().toUpperCase()) 
+				&& !c.getAddress().isEmpty() && c.getAddress().toUpperCase().equals(customer.getAddress().toUpperCase())).collect(Collectors.toList());
 		}
-		return foundCustomer;
+		return foundCustomers;
 	}
 }
--- library-server/src/test/java/de/h_da/library/datamanagement/usecase/impl/SearchImplTest.java
+++ library-server/src/test/java/de/h_da/library/datamanagement/usecase/impl/SearchImplTest.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 
 import de.h_da.library.configuration.LibraryTest;
 import de.h_da.library.datamanagement.entity.Book;
+import de.h_da.library.datamanagement.entity.Customer;
 import de.h_da.library.datamanagement.manager.BookManager;
 import de.h_da.library.datamanagement.manager.CustomerManager;
 import de.h_da.library.datamanagement.usecase.Search;
@@ -37,7 +38,7 @@ public class SearchImplTest extends LibraryTest{
 	
 	@Test
 	public void testFindBooksByAttributes() {
-		Book book1, book2, book3, book4, bookCreated1, bookCreated2, bookCreated3, bookCreated4;
+		Book book1, book2, book3;
 
         // preparation
         book1 = new Book();
@@ -51,36 +52,119 @@ public class SearchImplTest extends LibraryTest{
         book3 = new Book();
         book3.setAuthors("Schmitt");
         book3.setTitle("Die Wiese");
-        
-        book4 = new Book();
-        book4.setAuthors("Hallo");
-        book4.setTitle("Welt");
 
         // execution
-        bookCreated1 = bookManager.create(book1);
-        bookCreated2 = bookManager.create(book2);
-        bookCreated3 = bookManager.create(book3);
+        bookManager.create(book1);
+        bookManager.create(book2);
+        bookManager.create(book3);
         
         // evaluation
-        Book filter = new Book();
-        filter.setAuthors("Schmitt");
-        filter.setTitle("Der Hof");
-        ArrayList<Book> foundBooks = (ArrayList<Book>) search.findBooksByAttributes(filter);
+        Book filter1 = new Book();
+        filter1.setAuthors("Schmitt");
+        filter1.setTitle("Der Hof");
         
+        ArrayList<Book> foundBooks = (ArrayList<Book>) search.findBooksByAttributes(filter1);
+        
+        assertNotNull(foundBooks);
+        assertEquals(1,foundBooks.size());
+        assertEquals("filter1: book1 not found",foundBooks.get(0).getTitle()+foundBooks.get(0).getAuthors(),book1.getTitle()+book1.getAuthors());
+                
+        Book filter2 = new Book();
+        filter2.setTitle("Der Hof");
+        
+        ArrayList<Book> foundBooks2 = (ArrayList<Book>) search.findBooksByAttributes(filter2);
+        
+        assertNotNull(foundBooks2);
+        assertEquals(2,foundBooks2.size());
+        assertEquals("filter2: book1 not found",foundBooks2.get(0).getTitle()+foundBooks2.get(0).getAuthors(),book1.getTitle()+book1.getAuthors());
+        assertEquals("filter2: book2 not found",foundBooks2.get(1).getTitle()+foundBooks2.get(1).getAuthors(),book2.getTitle()+book2.getAuthors());
+              
+        
+        /*
         ArrayList<Book> expactedBooks = (ArrayList<Book>) bookManager.findAll();      
         
-        boolean result = foundBooks.containsAll(expactedBooks); 
         boolean match = false;
         
         for(Book found: foundBooks) {
         	for(Book exp: expactedBooks) {
-        		if(found.getTitle().equals(exp) && found.getAuthors().equals(exp.getAuthors())) {
+        		if(found.getTitle().equals(exp.getTitle()) && found.getAuthors().equals(exp.getAuthors())) {
         			match = true;
+        		}else {
+        			match = false;
         		}
         	}
         }
         
-        assertTrue("##### FAILURE: " + foundBooks.size() +" #####",match);
+        assertTrue("##### FAILURE: " + foundBooks.size() +" #####", match);
+        */
+	}
+	
+	@Test
+	public void testFindCustomersByAttributes() {
+		Customer customer1, customer2, customer3;
+
+        // preparation
+        customer1 = new Customer();
+        customer1.setName("Schmitt");
+        customer1.setAddress("Hinterhof 1"); 
+        
+        customer2 = new Customer();        
+        customer2.setName("Maier");
+        customer2.setAddress("Hinterhof 1");
+        
+        customer3 = new Customer();
+        customer3.setName("Schmitt");
+        customer3.setAddress("Wiesenstraße 42");
+
+        // execution
+        customerManager.create(customer1);
+        customerManager.create(customer2);
+        customerManager.create(customer3);
+        
+        // evaluation
+        /*
+        Customer filter = new Customer();
+        filter.setName("Schmitt");
+        filter.setAddress("Hinterhof 1");
+        
+        ArrayList<Customer> foundCustomers = (ArrayList<Customer>) search.findCustomersByAttributes(filter);
+        
+        ArrayList<Customer> expactedCustomers = (ArrayList<Customer>) customerManager.findAll();      
+        
+        boolean match = false;
+        
+        for(Customer found: foundCustomers) {
+        	for(Customer exp: expactedCustomers) {
+        		if(found.getName().equals(exp.getName()) && found.getAddress().equals(exp.getAddress())) {
+        			match = true;
+        		}else {
+        			match = false;
+        		}
+        	}
+        }
+        
+        assertTrue("##### FAILURE: " + foundCustomers.size() +" #####", match);
+        */
+        Customer filter1 = new Customer();
+        filter1.setName("Schmitt");
+        filter1.setAddress("Hinterhof 1");
+        
+        ArrayList<Customer> foundCustomers = (ArrayList<Customer>) search.findCustomersByAttributes(filter1);
+        
+        assertNotNull(foundCustomers);
+        assertEquals(1,foundCustomers.size());
+        assertEquals("filter1: customer1 not found",foundCustomers.get(0).getName()+foundCustomers.get(0).getAddress(),customer1.getName()+customer1.getAddress());
+        
+        
+        Customer filter2 = new Customer();
+        filter2.setAddress("Hinterhof 1");
+        
+        ArrayList<Customer> foundCustomers2 = (ArrayList<Customer>) search.findCustomersByAttributes(filter2);
+        
+        assertNotNull(foundCustomers2);
+        assertEquals(2,foundCustomers2.size());
+        assertEquals("filter2: customer1 not found",foundCustomers2.get(0).getName()+foundCustomers2.get(0).getAddress(),customer1.getName()+customer1.getAddress());
+        assertEquals("filter2: customer2 not found",foundCustomers2.get(1).getName()+foundCustomers2.get(1).getAddress(),customer2.getName()+customer2.getAddress());
         
 	}
 }
