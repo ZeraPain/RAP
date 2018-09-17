f8b16952a2258e0d9851bc45dd903f7adcf95ab0
dcal <caliskan88@web.de>
2018-09-17 17:11:02 +0200

    add frontend for reminder

--- library-client/src/main/java/de/h_da/library/client/reminder/ReminderController.java
+++ library-client/src/main/java/de/h_da/library/client/reminder/ReminderController.java
@@ -1,24 +1,71 @@
 package de.h_da.library.client.reminder;
 
+import de.h_da.library.LibraryException;
 import de.h_da.library.client.QuasarController;
+import de.h_da.library.client.ServerFacade;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 
-public class ReminderController implements QuasarController {
+import de.h_da.library.reminding.usecase.RemindingRemote;;
+
+
+public class ReminderController implements QuasarController  {
+
+	RemindingRemote reminderRemote;
+	   
+	
+	 @FXML
+    private Button sendAllReminders;
 
     @FXML
-    private Button refreshBtn;
+    private Button closeReminderBtn;
+    
+    @FXML
+    private TextArea textfield_id;
+    
 
     @FXML
-    void onBtnRefreshClicked(MouseEvent event) {
-    		System.out.println("clicked");
+    void closeReminderBtn(MouseEvent event) {
+    	String id = textfield_id.getText();
+    	
+    	System.out.println(id);
+    	
+    	long invoiceId = new Long( Integer.parseInt(id));
+    	
+    	try {
+			reminderRemote.closeReminder(invoiceId);
+		} catch (LibraryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+
+
+
+    @FXML
+    void sentReminderOutBtn(MouseEvent event) {
+
+    	try {
+			reminderRemote.sendReminders();
+		} catch (LibraryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
-		System.out.println("init reminder controller");
+		
+		reminderRemote = ServerFacade.getInstance().lookup("RemindingImpl", RemindingRemote.class);
+		
+		
+		
 	}
 
 }
